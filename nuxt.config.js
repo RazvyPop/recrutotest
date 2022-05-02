cb885b0a72f8d400e45c647966ffef9aead25de4
curvyx <47969516+RazvyPop@users.noreply.github.com>
2022-05-02 14:01:36 +0300

    title
    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - Big Five',
-    title: 'Free open-source BigFive personality traits test',
+    title: 'Recruto',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
